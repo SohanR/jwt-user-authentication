abc40bf04dc0202bb7e61f4d28141f453cf9e59c
sohanr <mizan.rahman66d@gmail.com>
2022-01-25 20:33:13 +0600

    exports

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,20 @@
-export.register = (req, res, next) => {
+exports.register = (req, res, next) => {
     res.send("Register Route")
 }
 
 
-export.login = (req, res, next) => {
+exports.login = (req, res, next) => {
     res.send("Login Route")
 }
 
 
-export.forgotPassword = (req, res, next) => {
+exports.forgotPassword = (req, res, next) => {
     res.send("Forgot Password Route")
 }
 
 
 
-export.resetPassword = (req, res, next) => {
+exports.resetPassword = (req, res, next) => {
     res.send("Reset Password Route")
 }
 
