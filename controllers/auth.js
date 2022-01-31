e75009f09553d7195654a34752143e3b15914b34
sohanr <mizan.rahman66d@gmail.com>
2022-02-01 00:51:32 +0600

    log in auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -23,8 +23,43 @@ exports.register = async (req, res, next) => {
 }
 
 
-exports.login = (req, res, next) => {
-    res.send("Login Route")
+exports.login = async (req, res, next) => {
+    const { email,password } = req.body;
+
+
+    // checking email or pass input field
+    if(!email || !password){
+        res.status(400).json({ success: false, error: "Please provide email and password and password" })
+    }
+
+    // checking the existence of that user
+
+
+    // finding email
+    try {
+        const user = await User.findOne({ email }).select("+password");
+
+        if(!user){
+            res.status(400).json({ success: false, error:"invalid credentials" })
+        }
+
+        // comparing password (user password matches or not)
+        const isMatch = await user.matchPassword(password);
+
+        if(!isMatch){
+            res.status(400).json({ success:false, error:"invalid credentials " });
+        }
+
+        res.status(200).json({
+            success:true,
+            token: "asdsgsdadg"
+        })
+
+
+    } catch (error) {
+        res.status(500).json({ success: false, error: error.message })
+    }
+
 }
 
 
