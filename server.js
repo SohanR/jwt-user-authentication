fac611abbfadaba12b7ef6c6c0c3594635c987c0
sohanr <mizan.rahman66d@gmail.com>
2022-01-31 23:42:28 +0600

    handle error rejection

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-const express = require('express');
-
 require('dotenv').config({path: './config.env'});
+const express = require('express');
 const connectDB  = require("./config/db");
 
 
@@ -17,6 +16,11 @@ app.use('/api/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 4000;
 
-app.listen( PORT, () => console.log(`server is running on port ${PORT}`))
+const server = app.listen( PORT, () => console.log(`server is running on port ${PORT}`))
 
 
+//handle rejection
+process.on("unhandledRejection", (err, promise) => {
+    console.log(`Logged Error: ${err}`);
+    server.close(() => process.exit(1));
+} )
